cef469aad0f1d6f73db31a2a67038b2e0fd49d9d
NicksyRop <nicksonkipkorir25@gmail.com>
2022-10-25 12:39:30 +0300

    added react-router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,26 @@ import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { Navbar } from "./layout/Navbar";
 import { Home } from "./Pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { AddUser } from "./User/AddUser";
+import { NotFound } from "./User/NotFound";
+import { EditUser } from "./User/EditUser";
+import { ViewUser } from "./User/ViewUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar />
-      <Home />
+      <Router>
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/add-user" element={<AddUser />} />
+          <Route path="/edit-user" element={<EditUser />} />
+          <Route path="/view-user" element={<ViewUser />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
